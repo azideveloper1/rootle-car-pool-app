95beb2d0d1f01f89d247585055b5cca9e829177b
Mehul Prajapati <mehul.prajapati@rootle.ai>
2025-11-13 10:21:51 +0530

    allow cors

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,14 @@
-
 const express = require('express');
 const connectDB = require('./config/db');
 const userRoutes = require('./routes/userRoutes');
+const cors = require('cors'); // Add this line
 
 const PORT = process.env.PORT || 3000;
 const HOST = process.env.HOST || "0.0.0.0";
 
 const app = express();
 app.use(express.json());
+app.use(cors()); // Allow CORS
 
 // Connect to MongoDB
 connectDB();
